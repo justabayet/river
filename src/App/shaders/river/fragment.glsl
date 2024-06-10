98b9a1154ae9f2a92a64b90e100ebeb589a5ad0d
justabayet <anthony.bayet@hotmail.com>
2024-06-11 01:32:21 +0200

    # smooth foam into water

--- src/App/shaders/river/fragment.glsl
+++ src/App/shaders/river/fragment.glsl
@@ -12,7 +12,7 @@ void main()
     vec3 water = mix(depthColor, surfaceColor, heightFactor);
     vec3 foam = vec3(1.0, 1.0, 1.0);
 
-    vec3 color = mix(water, foam, step(0.6, heightGroundEdge));
+    vec3 color = mix(water, foam, smoothstep(0.55, 0.6, heightGroundEdge));
     gl_FragColor = vec4(color, .8);
     // gl_FragColor = vec4(vec3(heightWaterEdge), .8);
     #include <colorspace_fragment>;
